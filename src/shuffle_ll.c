75e3b24416a0c5cc3a825e9072af4aaa0de25799
js395 <js395@st-andrews.ac.uk>
2019-03-07 21:58:27 +0000

    split implemented

--- src/shuffle_ll.c
+++ src/shuffle_ll.c
@@ -2,5 +2,11 @@
 
 Card* split(Card* deck, int size) {
   int mid = size/2;
+  Card* current_node = deck;
+  for (int i = 0; i < mid; i++) {
+    current_node = current_node->next;
+  }
+  Card* bottom_half = current_node->next;
+  current_node->next = NULL;
 
 }
