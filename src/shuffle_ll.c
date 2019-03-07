62e5a3feebfed6f38370eed68f606800db831f07
js395 <js395@st-andrews.ac.uk>
2019-03-07 22:21:36 +0000

    shuffle v0.1

--- src/shuffle_ll.c
+++ src/shuffle_ll.c
@@ -1,5 +1,10 @@
 #include "headers.h"
 
+/*
+@brief This function iterates through a linked list and splits it at the halfway point.
+then it returns the bottom half of the list.
+*/
+
 Card* split(Card* deck, int size) {
   int mid = size/2;
   Card* current_node = deck;
@@ -8,5 +13,19 @@ Card* split(Card* deck, int size) {
   }
   Card* bottom_half = current_node->next;
   current_node->next = NULL;
+  return bottom_half;
+}
 
+Card* shuffle(Card* top, Card* bot) {
+  Card* new_deck = top;
+  Card* current_node = top;
+  Card* top_ptr = top;
+  Card* bot_ptr = bot;
+  while (new_deck->next != NULL) {
+    top_ptr = top_ptr->next;
+    current_node->next = bot_ptr;
+    current_node = current_node->next;
+    bot_ptr = bot_ptr->next;
+    current_node->next = top_ptr;
+  }
 }
