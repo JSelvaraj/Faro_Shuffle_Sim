86e536702c48be92044ca71eab2479ba63fd5960
js395 <js395@st-andrews.ac.uk>
2019-03-07 20:47:30 +0000

    fixed seg fault bug

--- src/print_ll.c
+++ src/print_ll.c
@@ -4,20 +4,19 @@
 #include "headers.h"
 #include "files/print_faro_val.h"
 /*
-  @brief This function prints the deck of cards if when its a linked list
+@brief This function prints the deck of cards if when its a linked list
 */
 
 void print_deck(Card* deck, int size) {
   Card* current_node = deck;
-  char card[3] = {0,0,"\0"};
-  char* card_ptr = NULL;
+  char card[3] = {"","","\0"};
+  print_faro_val("", PREFIX_IN);
   while (size > 0) {
     card[0] = current_node->rank;
     card[1] = current_node->suit;
     current_node = current_node->next;
-    print_faro_val("", PREFIX_IN);
-    print_faro_val(*card_ptr, CARD_VAL);
-    print_faro_val("", SUFFIX);
+    print_faro_val(card, CARD_VAL);
     size--;
   }
+  print_faro_val("", SUFFIX);
 }
