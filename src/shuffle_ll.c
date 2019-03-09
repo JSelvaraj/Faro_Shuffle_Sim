7e267a605a4ac443e739ea585d7681c79cdbf886
JSelvaraj <selvarajjeffrey@yahoo.co.uk>
2019-03-09 17:00:47 +0000

    removed test commenting

--- src/shuffle_ll.c
+++ src/shuffle_ll.c
@@ -42,25 +42,25 @@ Card* split(Card* deck, int size) {
 Card* shuffle(Card* top, Card* bot, int size) {
   Card* new_deck = top;
   Card* current_node = top;
-  printf("Current Node Val %c%c\n", current_node->rank, current_node->suit);
+  // printf("Current Node Val %c%c\n", current_node->rank, current_node->suit);
   Card* top_ptr = top;
   Card* bot_ptr = bot;
   top_ptr = top_ptr->next;
   current_node->next = bot_ptr;
   current_node = current_node->next;
-  printf("Current Node Val %c%c\n", current_node->rank, current_node->suit);
+  // printf("Current Node Val %c%c\n", current_node->rank, current_node->suit);
   size -= 2;
   while (size > 0) {
     bot_ptr = bot_ptr->next;
     current_node->next = top_ptr;
     top_ptr = top_ptr->next;
     current_node = current_node->next;
-    printf("Current Node Val %c%c\n", current_node->rank, current_node->suit);
+    // printf("Current Node Val %c%c\n", current_node->rank, current_node->suit);
     current_node->next = bot_ptr;
     current_node = current_node->next;
-    printf("Current Node Val %c%c\n", current_node->rank, current_node->suit);
+    // printf("Current Node Val %c%c\n", current_node->rank, current_node->suit);
     size -= 2;
-    printf("%d\n", size);
+    // printf("%d\n", size);
   }
   return new_deck;
 }
