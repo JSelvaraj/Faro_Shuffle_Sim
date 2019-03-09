cfa17a704636279c193f9a074b5a9b4f5ab512ec
JSelvaraj <selvarajjeffrey@yahoo.co.uk>
2019-03-09 16:24:08 +0000

    test3

--- src/shuffle_ll.c
+++ src/shuffle_ll.c
@@ -21,7 +21,7 @@ Card* split(Card* deck, int size) {
   }
   Card* bottom_half = current_node->next;
   current_node->next = NULL;
-  print_deck(deck, size/2, PREFIX_IN);
+  print_deck(deck, size-1, PREFIX_IN);
     // print_deck(bottom_half, size/2, PREFIX_IN);
   return bottom_half;
 }
