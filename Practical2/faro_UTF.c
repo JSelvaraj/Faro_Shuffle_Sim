ad50795aab35e0fa2d4ac22c22bcfae603c60931
js395 <js395@st-andrews.ac.uk>
2019-03-11 17:53:25 +0000

    UTF compliant get cards

---
+++ Practical2/faro_UTF.c
@@ -0,0 +1,52 @@
+#include "headers.h"
+#include <limits.h>
+
+typedef struct Cart Cart;
+struct Cart {
+  char* value;
+  unsigned int number; //for the NUMERICAL extension
+  Cart* next;
+};
+
+
+
+
+Cart* get_cards_ranksuit(int size) {
+  char* string = malloc((sizeof(wchar_t) * size) + (size * sizeof(char) * 2));
+  fgets(string, (size * 5), stdin);
+  // printf("%s\n", string);
+  char* token = strtok(string, " ");
+
+  Cart* head = NULL;
+  Cart* current_node;
+  Cart* new_node;
+  while (token != NULL){
+    // printf("%s ", token );
+    new_node = malloc(sizeof(Cart));
+    new_node->value = token;
+    new_node->next = NULL;
+    if (head == NULL) {
+      head = new_node;
+      current_node = new_node;
+    } else {
+      current_node->next = new_node;
+      current_node = current_node->next;
+    }
+    token = strtok(NULL, " ");
+  }
+  return head;
+  printf("\n");
+}
+
+int main(void) {
+  int size = 6;
+  Cart* deck;
+  deck = get_cards(size);
+  print_faro_val("",PREFIX_IN);
+  for (int i = 0; i < size; i ++) {
+    print_faro_val(deck->value,CARD_VAL);
+    deck = deck->next;
+  }
+  print_faro_val("",SUFFIX);
+
+}
