fff7d1af640e869b32d29e9abc5541997fca1231
js395 <js395@st-andrews.ac.uk>
2019-03-11 15:33:12 +0000

    reorganised functions, no changes to functionality

--- Practical2/faro_ll.c
+++ Practical2/faro_ll.c
@@ -29,33 +29,6 @@ Card* get_cards_ranksuit(int size) {
   }
   return head;
 }
-/*
-@brief This function reads in a number of numeric values = to 'size' and returns the
-head of a linked list of the deck of cards
-
-@param size the number of values being read in.
-*/
-Card* get_cards_numerical(int size) {
-  Card* head = NULL;
-  Card* current_node;
-  Card *new_node;
-
-  for (int i = 0; i < size; i++) {
-    new_node = malloc(sizeof(Card));
-    scanf("%d\n", &new_node->number);
-    new_node->next = NULL;
-    if (head == NULL) {
-      head = new_node;
-      current_node = new_node;
-    } else {
-      current_node->next = new_node;
-      current_node = current_node->next;
-    }
-    if (i < (size -1)) {
-    }
-  }
-  return head;
-}
 /**
 @brief This function interweaves two equal length linked lists. Taking one
 node from top then from bottom, repeated until the end of the linked lists.
@@ -131,31 +104,6 @@ void print_deck(Card* current_node, int size, Stringplace prefix) {
   }
   print_faro_val("", SUFFIX);
 }
-/*
-@brief This function prints the deck of cards with numerical values
-
-@param deck the linked list of Cards being printed
-@param size the length of the linked list 'deck'
-@param prefix whether an IN shuffle or OUT shuffle was performed.
-
-*/
-void print_deck_numerical(Card* current_node, int size, Stringplace prefix) {
-  int length = getIntLength(current_node->number);
-  char* card = malloc(sizeof(char) * length);
-  for (int i = 0; i < length - 1; i++) {
-    card[i] = (char) "";
-  }
-  card[length - 1] = (char) NULL;
-  print_faro_val("", prefix);
-  while (size > 0) {
-    sprintf(card,"%d",current_node->number);
-    current_node = current_node->next;
-    print_faro_val(card, CARD_VAL);
-    size--;
-  }
-  print_faro_val("", SUFFIX);
-}
-
 /**
 @brief This function handles the faro shuffle by calling the other
 functions.
@@ -181,7 +129,6 @@ void faro(Card** deck,int size, int* k_binary, int k_length) {
     }
   }
 }
-
 /**
 @brief This function takes the head node of the linked list of cards and
 iteratively frees all the allocated memory from the list.
@@ -197,6 +144,7 @@ void free_ll(Card* current_node, int size) {
     size--;
   }
 }
+
 
 /**
 @brief This function handles the faro shuffle by calling the other
@@ -222,4 +170,55 @@ void faro_numerical(Card** deck,int size, int* k_binary, int k_length) {
       print_deck_numerical(*deck, size, PREFIX_OUT);
     }
   }
+}
+/*
+@brief This function prints the deck of cards with numerical values
+
+@param deck the linked list of Cards being printed
+@param size the length of the linked list 'deck'
+@param prefix whether an IN shuffle or OUT shuffle was performed.
+
+*/
+void print_deck_numerical(Card* current_node, int size, Stringplace prefix) {
+  int length = getIntLength(current_node->number);
+  char* card = malloc(sizeof(char) * length);
+  for (int i = 0; i < length - 1; i++) {
+    card[i] = (char) "";
+  }
+  card[length - 1] = (char) NULL;
+  print_faro_val("", prefix);
+  while (size > 0) {
+    sprintf(card,"%d",current_node->number);
+    current_node = current_node->next;
+    print_faro_val(card, CARD_VAL);
+    size--;
+  }
+  print_faro_val("", SUFFIX);
+}
+/*
+@brief This function reads in a number of numeric values = to 'size' and returns the
+head of a linked list of the deck of cards
+
+@param size the number of values being read in.
+*/
+Card* get_cards_numerical(int size) {
+  Card* head = NULL;
+  Card* current_node;
+  Card *new_node;
+
+  for (int i = 0; i < size; i++) {
+    new_node = malloc(sizeof(Card));
+    scanf("%d\n", &new_node->number);
+    new_node->next = NULL;
+    if (head == NULL) {
+      head = new_node;
+      current_node = new_node;
+    } else {
+      current_node->next = new_node;
+      current_node = current_node->next;
+    }
+    if (i < (size -1)) {
+    }
+  }
+  return head;
 }
